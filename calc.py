99aad2bf65df97e79f94bedee0131875ab876a14
joasramos1 <joasramos@cest.edu.br>
2018-09-06 18:33:03 +0000

    First commit

---
+++ calc.py
@@ -0,0 +1,13 @@
+def calc(n1, n2, op):
+	if op == '+':
+		return n1 + n2
+	elif op == '-':
+		return n1 - n2
+	elif op == '*':
+		return n1 * n2
+	elif op == '/':
+		if n2 == 0:
+			return 0
+		else:
+			return n1/n2
+print(calc(4,2,'/'))
---
+++ test_calc.py
@@ -0,0 +1,16 @@
+def calc(n1, n2, op):
+	if op == '+':
+		return n1 + n2
+	elif op == '-':
+		return n1 - n2
+	elif op == '*':
+		return n1 * n2
+	elif op == '/':
+		if n2 == 0:
+			return 0
+		else:
+			return n1/n2
+#print(calc(4,2,'/'))
+
+def test_calc():
+	assert calc(4,2,'+') == 5
